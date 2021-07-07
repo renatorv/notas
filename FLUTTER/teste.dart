790f91419a1f11c2fc94e9f8955a1364723c3ff5
unknown <renato@volus.com>
2021-07-07 10:04:28 -0300

    criar env python

--- FLUTTER/teste.dart
+++ FLUTTER/teste.dart
@@ -1,19 +1,19 @@
-class Produto {
-  String nome;
-  double preco;
+// class Produto {
+//   String nome;
+//   double preco;
 
-  //Construtor
-  // Produto(String nome, double preco) {
-  //   this.nome = nome;
-  //   this.preco = preco;
-  // }
-  Produto({this.nome, this.preco});
-}
+//   //Construtor
+//   // Produto(String nome, double preco) {
+//   //   this.nome = nome;
+//   //   this.preco = preco;
+//   // }
+//   Produto({this.nome, this.preco});
+// }
 
-main() {
-  var p1 = Produto(nome: 'mouse', preco: 28.99);
-  var p2 = Produto(preco: 22.99, nome: 'teclado');
+// main() {
+//   var p1 = Produto(nome: 'mouse', preco: 28.99);
+//   var p2 = Produto(preco: 22.99, nome: 'teclado');
 
-  print('Produto ${p1.nome} tem preço R\$ ${p1.preco}');
-  print('Produto ${p2.nome} tem preço R\$ ${p2.preco}');
-}
+//   print('Produto ${p1.nome} tem preço R\$ ${p1.preco}');
+//   print('Produto ${p2.nome} tem preço R\$ ${p2.preco}');
+// }
